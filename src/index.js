128a47b7b855087bb21d8e5beec4b719d3ae0515
SREENATH MP <sreenathmp95@gmail.com>
2020-04-07 19:46:32 +0530

    updated selected flag icon with globe icon

--- src/index.js
+++ src/index.js
@@ -141,7 +141,7 @@ class ReactFlagsSelect extends Component {
 				<div ref="selectedFlag" style={{fontSize: `${selectedSize}px`}} className={`selected--flag--option ${this.props.disabled ? 'no--focus' : ''}`} tabIndex="0" onClick={this.toggleOptions} onKeyUp={evt => this.toggleOptionsWithKeyboard(evt)}>
 					{isSelected &&
 						<span className="country-flag" style={{width: `${selectedSize}px`, height: `${selectedSize}px`}} >
-							<img src={require(`../flags/${isSelected.toLowerCase()}.svg`)} alt={isSelected}/>
+							<img src={require(`../flags/globe.svg`)} alt={isSelected}/>
 							{this.props.showSelectedLabel &&
 								<span className="country-label">{ this.props.customLabels[isSelected] || countries[isSelected] }</span>
 							}
